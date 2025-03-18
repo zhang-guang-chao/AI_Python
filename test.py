2f96a9ac55a61cbebc4b97922567485e9ca77a07
zhangguangchao <zhangguangchao@baijia.com>
2025-03-18 11:43:03 +0800

    langchain

---
+++ test.py
@@ -0,0 +1,12 @@
+def check_and_install_packages(packages):
+    for package in packages:
+        try:
+            import importlib
+            importlib.import_module(package)
+            print(f"{package} 库已安装。")
+        except ImportError:
+            print(f"{package} 库未安装，请使用 'pip3 install {package}' 进行安装。")
+
+# 使用该函数检查并安装指定的包
+packages_to_check = ['langchain', 'langchain_ollama']
+check_and_install_packages(packages_to_check)
